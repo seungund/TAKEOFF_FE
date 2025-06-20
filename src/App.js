114a735d5cd8137a551bccf8603203c5803d8a62
seungund <seungund@naver.com>
2025-06-20 18:08:18 +0900

    MOD 경로수정과 index.js 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Bar from '/Users/leeseungun/Documents/Develop/2025/takeoff_fe/src/components/Bar.js'
+import Bar from './components/Bar.js'
 import Display from './components/Display';
 
 function App() {
